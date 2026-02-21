0da605fc353c8040893f49d506d7ad90d21cf804
Amir <amirchegerchiev@gmail.com>
2026-02-21 23:36:44 +0300

    Fix project2

--- dirty_main.py
+++ dirty_main.py
@@ -1,8 +1,6 @@
-from datetime import date
-from application.salary import *
-from application.db.people import *
+from application.salary import calculate_salary
+from application.db.people import get_employees
 
 if __name__ == '__main__':
-    print("Текущая дата:", date.today().isoformat())
     employees = get_employees()
     calculate_salary(employees)
